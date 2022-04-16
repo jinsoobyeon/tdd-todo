a2f770e48249707d4092908f0f2c57d1431506b7
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-16 17:51:44 +0900

    Feat: 정의된 할 일 리스트의 값 렌더링

--- src/components/TodoItem.test.tsx
+++ src/components/TodoItem.test.tsx
@@ -3,11 +3,7 @@ import TodoItem from "./TodoItem";
 
 describe("List", () => {
   it("renders todos", () => {
-    const todos = [
-      { id: 1, todo: "할 일" },
-      { id: 2, todo: "투 두" },
-    ];
-    const { container } = render(<TodoItem todos={todos} />);
+    const { container } = render(<TodoItem />);
     expect(container).toHaveTextContent("할 일");
     expect(container).toHaveTextContent("투 두");
   });
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,22 +1,24 @@
-interface Todo {
-  id: number;
-  todo: string;
-}
+import React from "react";
 
-interface Todos {
-  todos?: Todo[];
-}
+export const todos = [
+  { id: 1, todo: "할 일" },
+  { id: 2, todo: "투 두" },
+];
 
-function TodoItem({ todos }: Todos) {
+function TodoItem() {
   return (
-    <li>
-      <div className="view">
-        <input className="toggle" type="checkbox" />
-        <label>할 일</label>
-        <button className="destroy" />
-      </div>
-      <input className="edit" />
-    </li>
+    <React.Fragment>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            <label>{todo.todo}</label>
+            <button className="destroy" />
+          </div>
+          <input className="edit" />
+        </li>
+      ))}
+    </React.Fragment>
   );
 }
 
