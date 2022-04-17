7d1b24a4e413e54ef9a8b2ce50800881629f90ab
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-17 23:00:34 +0900

    Test: input에서 value를 입력하고 'Enter' keyDown 시 value 렌더링

--- src/TDDTodo.test.tsx
+++ src/TDDTodo.test.tsx
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TDDTodo from "./TDDTodo";
 
 describe("TDDTodo", () => {
   it("renders tasks", () => {
     const { container } = render(<TDDTodo />);
+    const input = screen.getByPlaceholderText("What needs to be done?");
+    fireEvent.keyDown(input, { key: "Enter", target: { value: "할 일" } });
     expect(container).toHaveTextContent("할 일");
   });
 });
