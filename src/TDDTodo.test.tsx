3494efc6ce74aad0db5e9f7a5a369d242976e5bb
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-19 00:06:17 +0900

    Test: todos 키로 인풋 값을 로컬스토리지에 저장

--- src/TDDTodo.test.tsx
+++ src/TDDTodo.test.tsx
@@ -2,12 +2,17 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import TDDTodo from "./TDDTodo";
 
 describe("TDDTodo", () => {
-  it("renders tasks", () => {
-    const { container } = render(<TDDTodo />);
-    const input = screen.getByPlaceholderText("What needs to be done?");
-    fireEvent.change(input, { target: { value: "할 일" } });
-    fireEvent.keyDown(input, { key: "Enter" });
+  const { container } = render(<TDDTodo />);
+  const input = screen.getByPlaceholderText("What needs to be done?");
+  fireEvent.change(input, { target: { value: "할 일" } });
+  fireEvent.keyDown(input, { key: "Enter" });
+
+  it("renders todos", () => {
     expect(container).toHaveTextContent("할 일");
     expect(input).toHaveValue("");
+  });
+
+  it("set item localStorage", () => {
+    expect(window.localStorage.getItem("todos")).toMatch("할 일");
   });
 });
