a01f67a208bfccb31a59991d9d45a719e1ecf25f
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-22 11:12:40 +0900

    Test: 할 일 아이템을 더블 클릭하면 해당 할 일 값을 가지고 있는 인풋창으로 포커스된다.

--- src/TDDTodo.test.tsx
+++ src/TDDTodo.test.tsx
@@ -1,15 +1,33 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import TDDTodo from "./TDDTodo";
+import TodoItem from "./components/TodoItem";
 
-describe("create todos", () => {
-  const { container } = render(<TDDTodo />);
-  const input = screen.getByPlaceholderText("What needs to be done?");
-  fireEvent.change(input, { target: { value: "할 일" } });
-  fireEvent.keyPress(input, { charCode: 13 });
+describe("CRUD todo", () => {
+  it("init", () => {
+    render(<TDDTodo />);
+    const input = screen.getByPlaceholderText("What needs to be done?");
 
-  it("set item localStorage", () => {
+    expect(input).toHaveFocus();
+  });
+
+  it("create todo", () => {
+    const { container } = render(<TDDTodo />);
+    const input = screen.getByPlaceholderText("What needs to be done?");
+    fireEvent.change(input, { target: { value: "할 일" } });
+    fireEvent.keyPress(input, { charCode: 13 });
+
+    expect(input).toHaveFocus();
     expect(window.localStorage.getItem("todos")).toMatch("할 일");
     expect(input).toHaveValue("");
     expect(container).toHaveTextContent("할 일");
+  });
+
+  it("update todo", () => {
+    render(<TodoItem todos={[{ id: 1, todo: "할 일" }]} />);
+    const li = screen.getAllByRole("listitem");
+    const edit = screen.getByDisplayValue("할 일");
+    fireEvent.doubleClick(li[0]);
+
+    expect(edit).toHaveFocus();
   });
 });
