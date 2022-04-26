3dbc7a9f947ca78b4ba56ba25fcf75532731fdfe
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-26 11:16:41 +0900

    Test: 수정 인풋에서 엔터를 누르면 인풋 값 렌더링

--- src/components/TodoItem.test.tsx
+++ src/components/TodoItem.test.tsx
@@ -3,7 +3,9 @@ import TodoItem from "./TodoItem";
 
 describe("CRUD todo", () => {
   it("update todo", () => {
-    render(<TodoItem todos={[{ id: 1, todo: "할 일" }]} />);
+    const { container } = render(
+      <TodoItem todos={[{ id: 1, todo: "할 일" }]} />
+    );
     const li = screen.getAllByRole("listitem");
     const edit = screen.getByRole("textbox");
     fireEvent.doubleClick(li[0]);
@@ -14,5 +16,6 @@ describe("CRUD todo", () => {
     fireEvent.keyPress(edit, { charCode: 13 });
 
     expect(window.localStorage.getItem("todos")).toMatch("투 두");
+    expect(container).toHaveTextContent("투 두");
   });
 });
